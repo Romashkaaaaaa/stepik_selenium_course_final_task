3da20ea585366d4a8012449207e493a17cdc2503
Roman Ilichev <roman1999.06.11@gmail.com>
2023-03-26 20:11:56 +0300

    Add base files

---
+++ conftest.py
@@ -0,0 +1,38 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption("--language", action="store", default="en",
+                     help="Choose language: ru / en / fr / ...")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+
+    options_chrome = OptionsChrome()
+    options_chrome.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+
+    options_firefox = OptionsFirefox()
+    options_firefox.set_preference("intl.accept_languages", user_language)
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options = options_chrome)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(options = options_firefox)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser.implicitly_wait(3)
+    yield browser
+    print("\nquit browser..")
+    time.sleep(5)
+    browser.quit()
+#Пустя строчка снизу
