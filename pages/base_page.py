ae2b2fa070669f371685c114ee514e183cd70b7b
Romashkaaaaaa <91263614+Romashkaaaaaa@users.noreply.github.com>
2023-05-14 11:22:59 +0300

    Formatted the file according to pip8

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from .locators import BasePageLocators
 import math
 
+
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -18,7 +19,7 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return element.text
-    
+
     def go_to_basket_page(self):
         assert self.is_element_present(*BasePageLocators.BUSKET_LINK), "Basket link is not presented"
         link = self.browser.find_element(*BasePageLocators.BUSKET_LINK)
@@ -27,10 +28,10 @@ class BasePage():
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
-    
+
     def is_disappeared(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
@@ -42,7 +43,7 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
-    
+
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
@@ -52,12 +53,13 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
-    
+
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def should_be_authorized_user(self):
-        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented, probably unauthorised user"
+        assert self.is_element_present(
+            *BasePageLocators.USER_ICON), "User icon is not presented, probably unauthorised user"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -72,4 +74,3 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-#Пустая строчка снизу
